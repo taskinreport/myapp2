fa43d3f9767f908e490927360b1a2e9fdf277019
taskinreport <137386673+taskinreport@users.noreply.github.com>
2025-01-23 15:13:35 +0300

    önceki commite ek

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get 'pricing', to: 'pages#pricing', as: 'pricing'
   get 'contact', to: 'pages#contact', as: 'contact'
   get 'features', to: 'pages#features', as: 'features' #Yeni rota
+  get 'features', to: 'pages#features', as: 'features'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :students
